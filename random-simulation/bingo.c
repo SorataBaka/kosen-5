184f73464b8279f43748945d23a32262f92bc97c
Christian Harjuno <harjuno.tian@gmail.com>
2025-07-15 02:15:59 +0900

    finished all simulations

--- random-simulation/bingo.c
+++ random-simulation/bingo.c
@@ -2,10 +2,10 @@
 
 void simulateBingo(long long int repeat, Options opts)
 {
-  if (opts.has_outfile)
-    fprintf(opts.outfile, "repeat,valid,percentage\n");
   if (opts.bingomode == WITHIN)
   {
+    if (opts.has_outfile)
+      fprintf(opts.outfile, "repeat,valid,percentage\n");
     int valid = 0;
     for (long long int i = 0; i < repeat; i++)
     {
@@ -14,7 +14,12 @@ void simulateBingo(long long int repeat, Options opts)
       for (int j = 0; j < 7; j++)
       {
         simulateCalls(card, calledNumbers);
-        valid += validateCard(card);
+        int isvalid = validateCard(card);
+        if (isvalid == 1)
+        {
+          valid++;
+          break; // If the card is valid, we can stop calling numbers for this card
+        }
       }
       LOG("Repeat: %lld | Valid: %5d | Percentage: %06f\n", i + 1, valid, (double)valid / ((double)i + 1));
       if (opts.has_outfile)
@@ -45,27 +50,34 @@ void simulateBingo(long long int repeat, Options opts)
   else
   {
     if (opts.has_outfile)
-      fprintf(opts.outfile, "repeat,max_call,current_call_count,percentage");
+      fprintf(opts.outfile, "repeat,current_max,current_call_count,percentage\n");
     int current_max = 0;
+    int callCounts[76] = {0};
     for (long long int i = 0; i < repeat; i++)
     {
       BingoCard *card = generateHand();
-      int calls[76];
+
+      int calls[76] = {0};
+
       int callCount = 1;
       simulateCalls(card, calls);
       int valid = validateCard(card);
-      while (!valid)
+      while (valid != 1)
       {
         simulateCalls(card, calls);
         valid = validateCard(card);
         callCount++;
       }
+      callCounts[callCount]++;
       if (callCount > current_max)
         current_max = callCount;
 
-      LOG("Repeat: %lld | Current Max: %5d | Loop Count: %5d | Percentage: %0.06f\n", i + 1, current_max, callCount, (double)current_max / i + 1);
+      free(card);
+
+      LOG("Repeat: %lld | Current Max: %5d | Loop Count: %5d | Percentage: %0.06f\n", i + 1, current_max, callCount, (double)callCounts[callCount] / (i + 1));
+
       if (opts.has_outfile)
-        fprintf(opts.outfile, "%lld,%d,%d,%0.06f\n", i + 1, current_max, callCount, (double)current_max / i + 1);
+        fprintf(opts.outfile, "%lld,%d,%d,%0.06f\n", i + 1, current_max, callCount, (double)callCounts[callCount] / (i + 1));
     }
   }
 }
@@ -80,6 +92,12 @@ BingoCard *generateHand()
     int *row = generateWithinRangeWithoutRepetition(SIDE, lowLimit, upperLimit);
     for (int j = 0; j < SIDE; j++)
     {
+      if (i == 2 && j == 2) // Center space is free
+      {
+        (*card)[i][j].value = 0;  // Free space
+        (*card)[i][j].called = 1; // Mark as called
+        continue;
+      }
       (*card)[i][j].value = row[j];
       (*card)[i][j].called = 0;
     }
